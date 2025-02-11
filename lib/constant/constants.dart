0ea8b5b7a19b69a2d84198d88e3f9b8b61e75ed0
Wazid Hossain <mwh.wajid96@gmail.com>
2025-02-11 14:51:01 +0600

    movie model are created

---
+++ lib/constant/constants.dart
@@ -0,0 +1,7 @@
+const kmovieURL = "http://api.themoviedb.org/3/movie/";
+
+const kpopural = 'popular';
+const klatest = 'latest';
+const know_playing = 'now_playing';
+const ktop_rated = 'top_rated';
+const kupcoming = 'upcoming';
