71e1611d5f39a941234b44344fc105ad5aad4622
Wazid Hossain <mwh.wajid96@gmail.com>
2025-02-11 16:03:27 +0600

    now playing api is called

--- lib/UI/movie/movie_home.dart
+++ lib/UI/movie/movie_home.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_tmdb_api/model/movie_model.dart';
+import 'package:movie_tmdb_api/services/api_services.dart';
 
 class Movie_Home extends StatefulWidget {
   const Movie_Home({super.key});
@@ -10,13 +11,16 @@ class Movie_Home extends StatefulWidget {
 }
 
 class _Movie_HomeState extends State<Movie_Home> {
+  ApiServices apiServices = ApiServices();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
           FutureBuilder(
-            future: null, // Replace with your future
+            future: apiServices
+                .getMovieData(MovieType.nowPlaying), // Replace with your future
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 List<MovieModel> moviedata = snapshot.data ?? [];
@@ -38,6 +42,9 @@ class _Movie_HomeState extends State<Movie_Home> {
                   }).toList(),
                 );
               }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
             },
           )
         ],
